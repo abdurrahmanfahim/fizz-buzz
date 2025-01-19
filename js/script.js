16f2f66f97def7d937d8f5774ebeefaa6018b366
Abdur Rahman Fahim <127137394+abdurrahmanfahim@users.noreply.github.com>
2025-01-19 22:39:26 +0600

    added comment and cleared codes from unnessaccary things

--- js/script.js
+++ js/script.js
@@ -1,33 +1,31 @@
 // Write a function that prints numbers from 1 to 50. For multiples of 3, print "Fizz" instead of the number. For multiples of 5, print "Buzz". For numbers that are multiples of both 3 and 5, print "FizzBuzz".
 
-const btn = document.querySelector(".btn")
+// loop created for 50.
+for (let i = 1; i <= 50; i++ ) {
 
-btn.addEventListener("click", () => {
+    // checking conditions for modulus of 3 & 5 both.
+    if (i % 3 === 0 && i % 5 === 0) {
 
+        console.log("FizzBuzz!");
+        
+    }
+    // checking conditions for modulus of 3.
+    else if (i % 3 === 0) {
+        
+        console.log("Fizz");
+        
+    }
+    // checking conditions for modulus of 5.
+    else if (i % 5 === 0) {
 
-    for (let i = 1; i <= 50; i++ ) {
-    
-        if (i % 3 === 0 && i % 5 === 0) {
-
-            console.log("FizzBuzz!");
-            
-        }
-        else if (i % 3 === 0) {
-            
-            console.log("Fizz");
-            
-        }
-        else if (i % 5 === 0) {
-
-            console.log("Buzz!");
-            
-        }
-        else
-        {
-            console.log(i);
-            
-        }
+        console.log("Buzz!");
+        
+    }
+    // printing others there are not that by else.
+    else
+    {
+        console.log(i);
         
     }
     
-})
+}
